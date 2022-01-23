74dfc80dabd4fadc59c5107c343d4b8da960d8e4
Hyunwook Nam <hw0knam@gmail.com>
2022-01-24 03:24:32 +0900

    build: AWS SAM 셋업

--- main.py
+++ main.py
@@ -2,30 +2,40 @@ import os
 from dotenv import load_dotenv
 import requests
 import discord
+import json
+
 
 load_dotenv()
 
 ROTATION_API_KEY = os.getenv('ROTATION_API_KEY')
 DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 
-webhook = discord.Webhook.from_url(DISCORD_WEBHOOK_URL, adapter=discord.RequestsWebhookAdapter())
+def run(event, context):
+    webhook = discord.Webhook.from_url(DISCORD_WEBHOOK_URL, adapter=discord.RequestsWebhookAdapter())
 
-response = requests.get(f'https://api.mozambiquehe.re/crafting', params={'auth': ROTATION_API_KEY})
-bundles = response.json()
+    response = requests.get(f'https://api.mozambiquehe.re/crafting', params={'auth': ROTATION_API_KEY})
+    bundles = response.json()
 
-def run():
     for bundle in bundles:
         bundle_type = bundle['bundleType']
         bundle_contents = bundle['bundleContent']
         if bundle_type != 'daily' and bundle_type != 'weekly':
             continue
 
-    webhook.send(content=bundle_type)
+        embeds = []
+        for bundle_content in bundle_contents:
+            embed = discord.Embed()
+            embed.title = f'{bundle_content["itemType"]["name"]}({bundle_content["cost"]})'
+            embed.color = int(bundle_content["itemType"]["rarityHex"][1:], 16)
+            embed.set_image(url=bundle_content["itemType"]["asset"])
 
-    for bundle_content in bundle_contents:
-        embed = discord.Embed()
-        embed.title = f'{bundle_content["itemType"]["name"]}({bundle_content["cost"]})'
-        embed.color = int(bundle_content["itemType"]["rarityHex"][1:], 16)
-        embed.set_image(url=bundle_content["itemType"]["asset"])
+            embeds.append(embed)
+    
+        webhook.send(content=bundle_type, embeds=embeds)
 
-        webhook.send(embed=embed)
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello"
+        }),
+    }
